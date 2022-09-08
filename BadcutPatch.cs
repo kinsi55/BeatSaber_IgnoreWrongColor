e689b09f68b6e452f74616c64004927dd059f3a3
Kinsi <kinsi55@users.noreply.github.com>
2022-09-08 20:45:43 +0200

    fix

--- BadcutPatch.cs
+++ BadcutPatch.cs
@@ -11,10 +11,10 @@ using Zenject;
 namespace IgnoreWrongColor {
 	class BadcutPatch : IAffinity {
 		[Inject] Submission submission = null;
+		bool doAllowSubmission = false;
 
 		public BadcutPatch(bool doDisableSubmission) {
-			if(!doDisableSubmission)
-				submission = null;
+			doAllowSubmission = !doDisableSubmission;
 		}
 
 		[AffinityPrefix, AffinityPatch(typeof(GameNoteController), nameof(GameNoteController.HandleCut))]
@@ -22,7 +22,7 @@ namespace IgnoreWrongColor {
 			if(saber.saberType.MatchesColorType(__instance.noteData.colorType))
 				return true;
 			
-			if(submission != null) {
+			if(!doAllowSubmission && submission != null) {
 				submission.DisableScoreSubmission("IgnoreWrongColor");
 				submission = null;
 			}
